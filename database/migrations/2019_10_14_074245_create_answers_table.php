34d13cca0421297d0548d0572721bbc591b6c2ab
Stephen Fu <stephen.frx@gmail.com>
2019-10-14 15:50:04 +0800

    添加问卷问题、选项和答案模型

---
+++ database/migrations/2019_10_14_074245_create_answers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('survey_id');
+            $table->unsignedBigInteger('option_id')->nullable();
+            $table->unsignedBigInteger('questions_id');
+            $table->text('sugguestion')->nullable();
+            $table->integer('signed')->default(0)->comment('身份标志，0-教职工，1-学生');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
