600f3ae392f235f7b442a18af6859898aeb0b1b4
Stephen Fu <stephen.frx@gmail.com>
2019-10-15 18:04:55 +0800

    添加auth模块

---
+++ resources/views/survey.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">调查问卷</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    调查问卷项目列表
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
